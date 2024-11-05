b4162bde2b3c5333af25377763698f13661aef77
gaxpar <anndremarttins@gmail.com>
2024-11-05 19:52:53 -0300

    Projeto finalizado (até eu inventar algo pra implementar)

--- App.js
+++ App.js
@@ -1,36 +1,53 @@
-import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, FlatList} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 
 import api from './src/services/api';
 import Filmes from './src/Filmes';
 
 export default function gfilmes() {
-  const [filmes, setFilmes] = useState([])
+  const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
 
     async function loadFilmes() {
       const response = await api.get('r-api/?api=filmes');
       setFilmes(response.data)
+      setLoading(false)
     }
 
     loadFilmes();
 
   }, []);
 
- return (
-   <View style={styles.container}>
-    <FlatList
-    data={filmes}
-    keyExtractor={ item => String(item.id) }
-    renderItem={({ item }) => <Filmes data={item}/>}
-    />
-   </View>
-  );
+
+  if (loading) {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator color='#121212' size={45} />
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={filmes}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => <Filmes data={item} />}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
   },
+
+  loading: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
--- src/Filmes/index.js
+++ src/Filmes/index.js
@@ -1,10 +1,72 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native';
 
-export default function Filmes({data}) {
- return (
-   <View>
-    <Text> {data.nome} </Text>
-   </View>
-  );
+import Detalhes from '../Detalhes';
+
+export default function Filmes({ data }) {
+const [visibleModal, setVisibleModal] = useState(false)
+
+    return (
+        <View>
+            <View style={styles.card}>
+                <Text style={styles.titulo}> {data.nome} </Text>
+
+                <Image
+                    source={{ uri: data.foto }}
+                    style={styles.capa}
+                />
+
+                <View style={styles.areaBotao}>
+                    <TouchableOpacity style={styles.btn} onPress={() => setVisibleModal(true)}>
+                        <Text style={styles.btnText}>
+                            LEIA MAIS
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <Modal transparent={true} animationType='slide' visible={visibleModal}>
+                <Detalhes filme={data} voltar={() => setVisibleModal(false)}/>
+            </Modal>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: '#fff',
+        margin: 15,
+        elevation: 2,
+    },
+
+    capa: {
+        height: 250,
+        zIndex: 2,
+    },
+
+    titulo: {
+        padding: 15,
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+
+    areaBotao: {
+        alignItems: 'flex-end',
+        marginTop: -45,
+        zIndex: 9,
+    },
+    
+    btn:{
+        width: 100,
+        backgroundColor: '#09a6ff',
+        opacity: 1,
+        padding: 8,
+        borderTopLeftRadius: 7,
+        borderBottomLeftRadius: 7,
+    },
+
+    btnText:{
+        color: '#fff',
+        textAlign: 'center'
+    }
+})
